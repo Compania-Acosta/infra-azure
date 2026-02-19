89a3e7c92c3e9cf06c0dd1b08f565a1427f23f3f
Alex Acosta <alexanderx100x@gmail.com>
2026-02-18 22:13:08 -0500

    Cambio de apellido

--- envs/prd/backend.tf
+++ envs/prd/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-cicd-terraform-app-acosta" # Reemplazar por
     storage_account_name = "tfstateacosta"                # Reemplazar por 
-    container_name       = "tfstateacosta"
+    container_name       = "tfstate"
     key                  = "prd/terraform.tfstate"
   }
 }
